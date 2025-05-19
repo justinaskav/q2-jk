442c39b21dbacee0ad793e1db5b381f8862377e4
Justinas Kavoliūnas <justinaskav@gmail.com>
2025-05-19 08:44:53 +0200

    initial commit
    Created from https://github.com/caporaso-lab/cookiecutter-qiime2-plugin. See https://develop.qiime2.org to learn more.

---
+++ q2_jk/tests/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Justinas Kavoliūnas.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
---
+++ setup.py
@@ -0,0 +1,38 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Justinas Kavoliūnas.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+description = (
+    "Misc. tools."
+)
+
+setup(
+    name="q2-jk",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    author="Justinas Kavoliūnas",
+    author_email="justinaskav@gmail.com",
+    description=description,
+    url="https://github.com/justinaskav/q2-jk",
+    entry_points={
+        "qiime2.plugins": [
+            "q2_jk="
+            "q2_jk"
+            ".plugin_setup:plugin"]
+    },
+    package_data={
+        "q2_jk": ["citations.bib"],
+        "q2_jk.tests": ["data/*"],
+    },
+    zip_safe=False,
+)
